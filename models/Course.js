a4426561cb0e602a8d9bcd89c1480620b80434a6
merve kartal <mervekartal@merves-MacBook-Air.local>
2023-01-02 13:00:32 +0300

    slugify module added to create slug for id

--- models/Course.js
+++ models/Course.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const slugify = require('slugify')
 const Schema = mongoose.Schema
 
 const CourseSchema = new Schema({
@@ -15,8 +16,22 @@ const CourseSchema = new Schema({
     createdAt: {
         type: Date,
         default: Date.now
+    },
+    slug: {
+        type: String,
+        unique: true
     }
 })
+
+//url'de id göndermek yerine kurs adını kullanarak bir slug oluşturup slug'ı gönderdik.
+CourseSchema.pre('validate', function(next){
+    this.slug = slugify(this.name, {
+        lower: true,
+        strict: true
+    })
+    next()
+})
+
 
 const Course = mongoose.model('Course',CourseSchema)
 module.exports = Course
