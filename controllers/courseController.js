d6656598f225bd03cf5efa6a024dfddabf1ae526
merve kartal <mervekartal@merves-MacBook-Air.local>
2023-01-02 10:31:13 +0300

    course routes and pages modified

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -3,17 +3,47 @@ const Course = require('../models/Course')
 
 exports.createCourse = async (req,res) => {
     //template hazır olmadığı için json dosyası gönderildi.
-    const course = await Course.create(req.body)
     try{
+    const course = await Course.create(req.body)
         res.status(201).json({
             status: 'success',
             course: course
         })
         //res.send('Yeni kurs oluşturuldu')
-    }catch{
+    }catch(err){
         res.status(400).json({
             status: 'fail',
             error
         })
     }
+}
+
+exports.getAllCourses = async (req,res) => {
+
+    try{
+    const courses = await Course.find()
+        res.status(200).render('courses', {
+            courses,
+            page_name: "courses"
+        })
+    }catch(err){
+         res.status(400).json({
+         status: 'fail',
+         error
+
+        })
+    }
+    //template hazır olmadığı için json dosyası gönderildi.
+    // try{
+    // const courses = await Course.find()
+    //     res.status(200).json({
+    //         status: 'success',
+    //         courses: courses
+    //     })
+    // }catch(err){
+    //     res.status(400).json({
+    //         status: 'fail',
+    //         error
+    //     })
+    // }
 }
