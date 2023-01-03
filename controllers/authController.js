0d63b008e385b2d4a6f85e5cf00a0d20f039a4f6
merve kartal <mervekartal@merves-MacBook-Air.local>
2023-01-03 14:19:01 +0300

    login operations

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 
+const bcrypt = require('bcrypt')
 const User = require('../models/User')
 
 exports.createUser = async (req,res) => {
@@ -8,6 +9,29 @@ exports.createUser = async (req,res) => {
             status: 'success',
             user
         })
+    }catch(error){
+        res.status(400).json({
+            status: 'fail',
+            error
+        })
+    }
+}
+
+exports.loginUser = async (req,res) => {
+    try{
+
+        const { email, password } = req.body
+
+        let user = await User.findOne({ email })
+
+        let same = await bcrypt.compare(password, user.password)
+
+        if(same){ 
+            res.status(200).send('Login successful')
+        } else{ 
+            res.send('Geçersiz')
+        }        
+
     }catch(error){
         res.status(400).json({
             status: 'fail',
