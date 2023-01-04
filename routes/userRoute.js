60f8a50dcbe7785dcb9643bcf44fd603d521526f
merve kartal <mervekartal@merves-MacBook-Air.local>
2023-01-04 16:11:48 +0300

    custom middlewares added for page routes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const authController = require('../controllers/authController')
+const authMiddleware = require('../middlewares/authMiddleware')
 
 const router = express.Router()
 
 router.route('/signup').post(authController.createUser) //http://localhost:3000/users/signup
 router.route('/login').post(authController.loginUser) 
 router.route('/logout').get(authController.logoutUser) 
-router.route('/dashboard').get(authController.getDashboardPage) 
+router.route('/dashboard').get(authMiddleware, authController.getDashboardPage) //url üzerinden dashboard sayfasına gitmek isteyen kullanıcılar, giriş yapmadıysa login sayfasına yönlendirilir. 
+
 
 module.exports = router
